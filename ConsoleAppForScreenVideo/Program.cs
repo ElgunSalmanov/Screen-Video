11aa18f4df758a500132ca8b6675347cf02104c7
ElgunSalmanov <139856247+ElgunSalmanov@users.noreply.github.com>
2023-08-13 14:27:29 +0400

    Screen-Video

---
+++ ConsoleAppForScreenVideo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleAppForScreenVideo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipAddress = IPAddress.Loopback;
+
+            var listener = new TcpListener(ipAddress, 27001);
+
+            listener.Start();
+
+            var endPoint = "";
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var stream = client.GetStream();
+                var br = new BinaryReader(stream);
+
+                Bitmap bitmap;
+
+                while (true)
+                {
+                    var message = br.ReadString();
+
+                    if (message == "Screen")
+                    {
+                        endPoint = (client.Client.RemoteEndPoint as IPEndPoint).Address.ToString();
+                        bitmap = new Bitmap(1366, 768);
+
+                        Size size = new Size(bitmap.Width, bitmap.Height);
+
+                        Graphics memoryGraphics = Graphics.FromImage(bitmap);
+                        memoryGraphics.CopyFromScreen(0, 0, 0, 0, size);
+
+                        UdpClient udpClient = new UdpClient();
+
+                        ImageConverter imageConverter = new ImageConverter();
+
+                        var bytes = (byte[])imageConverter.ConvertTo(bitmap, typeof(byte[]));
+
+                        int bytesLength = (bytes.Length / 10000) + 1;
+                        int count = 0;
+
+                        while (bytesLength >= 0)
+                        {
+                            if (bytesLength == 0)
+                            {
+                                udpClient.Send(new byte[2] { 2, 3 }, new byte[2] { 2, 3 }.Length, IPAddress.Loopback.ToString(), 27001);
+                                break;
+                            }
+
+                            udpClient.Send(bytes.Skip(count).Take(20000).ToArray(), bytes.Skip(count).Take(10000).ToArray().Length, endPoint.ToString(), 27001);
+
+                            bytesLength--;
+                            count += 10000;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ScreenVideo/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace ScreenVideo
+{
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Tick += Screen;
+            timer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        private void screenbtn_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+
+            tbox.IsReadOnly = true;
+            screenbtn.IsEnabled = false;
+        }
+
+        public void Screen(object? sender, EventArgs e)
+        {
+            var tcpClient = new TcpClient();
+
+            tcpClient.Connect(tbox.Text, 27001);
+
+            var stream = tcpClient.GetStream();
+            var bw = new BinaryWriter(stream);
+
+            bw.Write("Screen");
+
+            tcpClient.GetStream().Close();
+            tcpClient.Close();
+
+            List<byte> listbyte = new List<byte>();
+
+            UdpClient udpClient = new UdpClient(27001);
+
+            var endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                byte[] buffer = udpClient.Receive(ref endPoint);
+
+                if (buffer.Length == 2) break;
+
+                foreach (var item in buffer)
+                {
+                    listbyte.Add(item);
+                }
+            }
+
+            byte[] count = new byte[listbyte.Count];
+
+            listbyte.CopyTo(count);
+
+            image.Source = byteImage(count);
+
+            udpClient.Close();
+        }
+
+        public static ImageSource byteImage(byte[] image)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+
+            MemoryStream memoryStream = new MemoryStream(image);
+
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = memoryStream;
+            bitmapImage.EndInit();
+
+            ImageSource imageSource = bitmapImage as ImageSource;
+
+            return imageSource;
+        }
+    }
+}
